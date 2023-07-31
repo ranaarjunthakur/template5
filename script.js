f47cf2dea771df13acc8c8b4b278daa79d246c50
ARJUN PRATAP RANA <89635484+ranaarjunthakur@users.noreply.github.com>
2023-07-31 11:41:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+
+  // function toggleNav() {
+  //   const navLinks = document.querySelector('.navPart2');
+  //   let menuIcon = document.querySelector('.ri-menu-4-fill')
+  //   let closeIcon = document.querySelector('.ri-close-fill')
+  
+  //   if (navLinks.style.display === 'none') {
+  //     navLinks.style.display = 'block';
+  //     menuIcon.style.display = 'none';
+  //     closeIcon.style.display = 'block';
+  //   } else {
+  //     navLinks.style.display = 'none';
+  //     menuIcon.style.display = 'block';
+  //     closeIcon.style.display = 'inline-block';
+  //   }
+  // }
